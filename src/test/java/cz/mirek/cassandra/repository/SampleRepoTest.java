c0d2af83f56f5a6bb579196a55283a2d68074be4
mirekrousal@centrum.cz <mirekrousal@centrum.cz>
2018-03-02 21:01:12 +0000

    Annotations used for the Cassandra init

--- src/test/java/cz/mirek/cassandra/repository/SampleRepoTest.java
+++ src/test/java/cz/mirek/cassandra/repository/SampleRepoTest.java
@@ -1,19 +1,18 @@
 package cz.mirek.cassandra.repository;
 
-import com.datastax.driver.core.Session;
 import cz.mirek.cassandra.CassandraApplication;
 import cz.mirek.cassandra.domain.Sample;
-import org.apache.thrift.transport.TTransportException;
-import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
-import org.junit.BeforeClass;
+import org.cassandraunit.spring.CassandraDataSet;
+import org.cassandraunit.spring.CassandraUnit;
+import org.cassandraunit.spring.CassandraUnitDependencyInjectionTestExecutionListener;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.IOException;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
@@ -23,19 +22,12 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CassandraApplication.class)
 @ActiveProfiles("test")
+@TestExecutionListeners({ CassandraUnitDependencyInjectionTestExecutionListener.class, DependencyInjectionTestExecutionListener.class })
+@CassandraDataSet(value = { "cql/sample-table.cql", "cql/sample-tabledata.cql" }, keyspace = "mirek")
+@CassandraUnit
 public class SampleRepoTest {
     @Autowired
     private SampleRepo sampleRepo;
-
-    @BeforeClass
-    public static void initDb() throws IOException, TTransportException {
-        EmbeddedCassandraServerHelper.startEmbeddedCassandra("cassandra.yaml", 20000);
-
-        Session session = EmbeddedCassandraServerHelper.getCluster().connect();
-        session.execute("create keyspace  \"mirek\" WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};");
-        session.execute("create table mirek.sample (id VARCHAR, PRIMARY KEY (id))");
-        session.execute("insert into mirek.sample (id) values ('test')");
-    }
 
     @Test
     public void shouldHaveSampleRepo() {
